85414bfae48be823c9d1e8ece64914cadd2a61ba
Shaishav <shaishavsharma.work@gmail.com>
2024-08-12 10:38:46 +0530

    Mini Project# 3

---
+++ 20240812/test_MiniProject3.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import pytest
+import allure
+
+
+def test_mini_project3():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.idrive360.com/enterprise/login")
+
+    user_id = driver.find_element(By.ID,"username")
+    user_id.send_keys("augtest_040823@idrive.com")
+
+    pswd = driver.find_element(By.ID, "password")
+    pswd.send_keys("123456")
+
+    sign_in = driver.find_element(By.ID,"frm-btn")
+    sign_in.click()
+    time.sleep(20)
+
+    assert driver.current_url == "https://www.idrive360.com/enterprise/account?upgradenow=true"
+    upgrade_now = driver.find_element(By.ID, "upgrade")
+    upgrade_now.click()
+    time.sleep(10)
+    upgrade_noww = driver.find_element(By.CLASS_NAME,"id-card-title")
+    assert upgrade_noww.is_displayed()
