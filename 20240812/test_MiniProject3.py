c79bc22048cea41d364208444e6709d7d8c8ba1b
Shaishav <shaishavsharma.work@gmail.com>
2024-08-12 11:39:33 +0530

    Mini Project# 3

--- 20240812/test_MiniProject3.py
+++ 20240812/test_MiniProject3.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 import pytest
 import allure
+import allure_pytest
 
 @allure.title("Mini Project# 3")
 @allure.description("Verify that if user validity is expired and needs to upgrade")
@@ -11,7 +12,6 @@ def test_mini_project3():
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get("https://www.idrive360.com/enterprise/login")
-
     user_id = driver.find_element(By.ID,"username")
     user_id.send_keys("augtest_040823@idrive.com")
 
@@ -23,8 +23,10 @@ def test_mini_project3():
     time.sleep(20)
 
     assert driver.current_url == "https://www.idrive360.com/enterprise/account?upgradenow=true"
+    allure.attach(driver.get_screenshot_as_png(),name='Screenshot')
     upgrade_now = driver.find_element(By.ID, "upgrade")
     upgrade_now.click()
     time.sleep(10)
     upgrade_noww = driver.find_element(By.CLASS_NAME,"id-card-title")
     assert upgrade_noww.is_displayed()
+    allure.attach(driver.get_screenshot_as_png(), name='upgrade_Screenshot')
